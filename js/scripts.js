d44befb5fe348f7408ad5b93f76f073c798bde94
naiyoma <lankas.aurelia@gmail.com>
2018-07-04 16:36:10 +0300

    conversion updates

--- js/scripts.js
+++ js/scripts.js
@@ -36,11 +36,19 @@ return number1/number2;
 var number1 = parseInt(prompt("Kindly Enter your mass in kgs"));
 var number2 = parseInt(prompt("Kindly Enter your height in metres"));
 var result = division(number1,number2);
-alert(result);
+alert(result);//BMI calculator//
 
 var multiply = function(number1) {
 		return number1 * 8.2;
 };
 var number1 = parseInt(prompt("Kindly Enter your temperature in degrees Celcius"));
 var result = multiply(number1,8.2);
-alert(result);
+alert(result); //conversion of Celcius to fahrenheit//
+
+
+var multiply = function(number1) {
+return number1 * 3.78541;
+};
+var number1 = parseInt(prompt("Kindly Enter your quantity in Gallons:"));
+var result = multiply(number1, 3.78541);
+alert(result);//conversion of gallons to litres//
