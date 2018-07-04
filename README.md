c8730edd58d36ee1d5a9b7a3eb7c297fcf4ac6e0
naiyoma <lankas.aurelia@gmail.com>
2018-07-04 14:56:50 +0300

    added the BMI

---
+++ README.md
@@ -0,0 +1,22 @@
+# Title
+Calculator project
+#### Description
+A calculator application based on simple arithmetics
+#### Authors
+-Lewis Mutuma
+-Naiyoma Aurelia
+## Setup/Installation Requirements
+* Github
+* Requires Google Chrome
+## Known Bugs
+Doesnt work on explorer
+## Technologies Used
+html
+css
+js
+## Support and contact details
+-Lewis
+-Naiyoma
+### License
+MIT
+Copyright (c) 2018 Lewis Mutuma
---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+var add = function(number1,number2) {
+return number1+number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = add(number1, number2);
+alert(result);
+
+var minus = function(number1,number2) {
+  return number1-number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = minus(number1,number2);
+alert(result);
+
+var divide = function(number1,number2) {
+  return number1/number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = divide(number1,number2);
+alert(result);
+
+var multiply = function(number1,number2) {
+  return number1*number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = multiply(number1,number2);
+alert(result);
+
+var division = function(number1,number2) {
+return number1/number2;
+};
+var number1 = parseInt(prompt("Kindly Enter your mass in kgs"));
+var number2 = parseInt(prompt("Kindly Enter your height in metres"));
+var result = division(number1,number2);
+alert(result);
+
+var multiply = function(number1) {
+		return number1 * 8.2;
+};
+var number1 = parseInt(prompt("Kindly Enter your temperature in degrees Celcius"));
+var result = multiply(number1,8.2);
+alert(result);
